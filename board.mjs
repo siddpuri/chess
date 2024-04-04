f1bf8e9eceb587ac52575465c6f1e11234cc081e
Sidd Puri <sidd@studiopuri.com>
2024-04-03 22:41:41 -0700

    Implement basic moves

--- board.mjs
+++ board.mjs
@@ -2,11 +2,32 @@ import Piece from './piece.mjs';
 
 export default class Board {
     board = new Array(8).fill(null).map(() => new Array(8).fill(null));
+    moves = [];
 
     Board() {
         createPieces();
     }
 
     createPieces() {
+        for (let i = 0; i < 8; i++) {
+            this.board[1][i] = new Pawn(this, 1, [1, i]);
+            this.board[6][i] = new Pawn(this, -1, [6, i]);
+        }
+        this.board[0][0] = new Rook(this, 1, [0, 0]);
+        this.board[0][7] = new Rook(this, 1, [0, 7]);
+        this.board[7][0] = new Rook(this, -1, [7, 0]);
+        this.board[7][7] = new Rook(this, -1, [7, 7]);
+        this.board[0][1] = new Knight(this, 1, [0, 1]);
+        this.board[0][6] = new Knight(this, 1, [0, 6]);
+        this.board[7][1] = new Knight(this, -1, [7, 1]);
+        this.board[7][6] = new Knight(this, -1, [7, 6]);
+        this.board[0][2] = new Bishop(this, 1, [0, 2]);
+        this.board[0][5] = new Bishop(this, 1, [0, 5]);
+        this.board[7][2] = new Bishop(this, -1, [7, 2]);
+        this.board[7][5] = new Bishop(this, -1, [7, 5]);
+        this.board[0][3] = new Queen(this, 1, [0, 3]);
+        this.board[7][3] = new Queen(this, -1, [7, 3]);
+        this.board[0][4] = new King(this, 1, [0, 4]);
+        this.board[7][4] = new King(this, -1, [7, 4]);
     }
 }
--- piece.mjs
+++ piece.mjs
@@ -1,3 +1,8 @@
+const horizontals = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+const diagonals = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
+const alldirs = concat(horizontals, diagonals);
+const knights = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]];
+
 export default class Piece {
     board;
     color;
@@ -17,12 +22,24 @@ export default class Piece {
         }
         return this.board[row][col];
     }
+
+    getLongMoves(dirs) {
+        let moves = [];
+        for (let dir of dirs) {
+            let [r, c] = dir;
+            for (let target = null; target === null; r += dir[0], c += dir[1]) {
+                target = whatsAt(r, c);
+                if (target === null || (target && target.color !== this.color)) {
+                    moves.push([r, c]);
+                }
+            }
+        }
+        return moves;
+    }
 }
 
 export class Pawn extends Piece {
-    constructor(board, color, position) {
-        super(board, color, position);
-    }
+    constructor(board, color, position) { super(board, color, position); }
 
     getValue() { return 1; }
 
@@ -41,9 +58,72 @@ export class Pawn extends Piece {
             }
         }
         // TODO: en passant
+        return moves;
     }
 
     isInStartingRow() {
         return this.color === 1 ? this.position[0] === 1 : this.position[0] === 6;
+    }
+}
+
+export class Rook extends Piece {
+    constructor(board, color, position) { super(board, color, position); }
+
+    getValue() { return 5; }
+    getMoves() { return getLongMoves(horizontals); }
+}
+
+export class Knight extends Piece {
+    constructor(board, color, position) { super(board, color, position); }
+
+    getValue() { return 3; }
+
+    getMoves() {
+        let moves = [];
+        for (let offset of knights) {
+            let target = whatsAt(offset[0], offset[1]);
+            if (target === null || (target && target.color !== this.color)) {
+                moves.push(offset);
+            }
+        }
+        return moves;
+    }
+}
+
+export class Bishop extends Piece {
+    constructor(board, color, position) {
+        super(board, color, position);
+    }
+
+    getValue() { return 3; }
+    getMoves() { return getLongMoves(diagonals); }
+}
+
+export class Queen extends Piece {
+    constructor(board, color, position) {
+        super(board, color, position);
+    }
+
+    getValue() { return 9; }
+    getMoves() { return getLongMoves(alldirs); }
+}
+
+export class King extends Piece {
+    constructor(board, color, position) {
+        super(board, color, position);
+    }
+
+    getValue() { return 1000; }
+
+    getMoves() {
+        let moves = [];
+        for (let offset of alldirs) {
+            let target = whatsAt(offset[0], offset[1]);
+            if (target === null || (target && target.color !== this.color)) {
+                moves.push(offset);
+            }
+        }
+        // TODO: castling
+        return moves;
     }
 }
